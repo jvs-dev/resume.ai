e0c4924dbfda0faa0af5ca21d0fc01dfd71a8350
joão vitor <shepherdcom12@gmail.com>
2024-02-03 23:50:17 -0300

    test 2

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  // ... outras configurações
+  server: {
+    proxy: {
+      '/api': 'http://localhost:3000', // exemplo de configuração de proxy
+    },
+  },
+  // configuração de rotas
+  routes: [
+    { "handle": "filesystem" },
+    { "src": "/.*", "dest": "/index.html" }
+  ],
+});
